867f3741069ae810251efe5b4bf09d9217152380
Morgan <mrgnhnt96@gmail.com>
2025-05-02 13:16:52 -0700

    feat: add tests for init command

--- test/e2e/init_command_test.dart
+++ test/e2e/init_command_test.dart
@@ -34,6 +34,18 @@ void main() {
       when(() => logger.err(any())).thenAnswer((e) {
         print(e.positionalArguments.first);
       });
+
+      when(
+        () => logger.confirm(
+          'The directory ~/secrets/.pnv already exists. Do you still want to use it?',
+        ),
+      ).thenReturn(true);
+
+      when(
+        () => logger.confirm(
+          'Do you want to add more flavors?',
+        ),
+      ).thenReturn(false);
     });
 
     group('successfully finishes', () {
@@ -75,7 +87,6 @@ name: bananas
         );
 
         expect(getConfig(), expected);
-        expect(exitCode, 0);
       });
 
       test('and creates a new config with existing flavors', () async {
@@ -96,7 +107,91 @@ name: bananas
         );
 
         expect(getConfig(), expected);
-        expect(exitCode, 0);
+      });
+
+      test(
+          'and creates a new key if the config contains a flavor '
+          'that does not have a keys', () async {
+        fs.file('.pnvrc')
+          ..createSync()
+          ..writeAsStringSync(
+            jsonEncode(
+              PnvConfig(
+                storage: '~/secrets/.pnv',
+                flavors: const {
+                  'local': [],
+                },
+              ).toJson(),
+            ),
+          );
+
+        await run();
+
+        final expected = PnvConfig(
+          storage: '~/secrets/.pnv',
+          flavors: const {
+            'local': [],
+          },
+        );
+
+        expect(getConfig(), expected);
+      });
+
+      test('and can create a new flavor when flavors already exist', () async {
+        final dir = fs.directory('$home/secrets/.pnv')
+          ..createSync(recursive: true);
+
+        dir.childFile('local.key').createSync();
+
+        when(
+          () => logger.confirm(
+            'Do you want to add more flavors?',
+          ),
+        ).thenReturn(true);
+
+        final answers = ['new-flavor', ''];
+
+        when(
+          () => logger.prompt(
+            "What's the name of the new Flavor? (leave blank to finish)",
+          ),
+        ).thenAnswer((_) => answers.removeAt(0));
+
+        await run();
+
+        final expected = PnvConfig(
+          storage: '~/secrets/.pnv',
+          flavors: const {
+            'local': [],
+            'new-flavor': [],
+          },
+        );
+
+        expect(getConfig(), expected);
+      });
+
+      test('and can create flavors when no flavors exist', () async {
+        fs.directory('$home/secrets/.pnv').createSync(recursive: true);
+
+        final answers = ['new-flavor', 'new-flavor-2', ''];
+
+        when(
+          () => logger.prompt(
+            "What's the name of the new Flavor? (leave blank to finish)",
+          ),
+        ).thenAnswer((_) => answers.removeAt(0));
+
+        await run();
+
+        final expected = PnvConfig(
+          storage: '~/secrets/.pnv',
+          flavors: const {
+            'new-flavor': [],
+            'new-flavor-2': [],
+          },
+        );
+
+        expect(getConfig(), expected);
       });
     });
   });
